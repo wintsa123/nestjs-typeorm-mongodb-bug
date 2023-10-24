db75370abaae0b77a1e9754fd9eadaf0dbf270f0
wintsa <wintsa123@gmail.com>
2023-10-24 10:06:41 +0800

    try

--- src/main.ts
+++ src/main.ts
@@ -13,44 +13,18 @@ import fastifyCookie from '@fastify/cookie';
 export const config = getConfig();
 const PORT = config.PORT || 9001;
 const PREFIX = config.PREFIX || '/';
-async function bootstrap() {
-  const logger: Logger = new Logger('main.ts');
-
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
-    logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
-  });
-  app.enableCors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  });
-  // 启动版本管理
-  app.enableVersioning({
-    defaultVersion: '1', // 不指定默认版本为v1
-    type: VersioningType.URI,
-  });
-
-  // 给请求添加prefix
-  app.register(fastifyCookie, {
-    secret: 'zw', // for cookies signature
-  });
-  app.register(fastifyCsrf);
-
-  app.setGlobalPrefix(PREFIX);
-  await app.listen(PORT, () => {
-    logger.log(`服务已经启动,接口请访问:http://wwww.localhost:${PORT}/${PREFIX}`);
-  });
-}
 // async function bootstrap() {
 //   const logger: Logger = new Logger('main.ts');
-//   //暂时关闭了api接口
-// const app = await NestFactory.create( AppModule , {
-//   // 开启日志级别打印
-//   logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
-// });
-//   //允许跨域请求
-//   app.enableCors();
+
+//   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+//     logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
+//   });
+//   app.enableCors({
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   });
 //   // 启动版本管理
 //   app.enableVersioning({
 //     defaultVersion: '1', // 不指定默认版本为v1
@@ -58,9 +32,35 @@ async function bootstrap() {
 //   });
 
 //   // 给请求添加prefix
+//   app.register(fastifyCookie, {
+//     secret: 'zw', // for cookies signature
+//   });
+//   app.register(fastifyCsrf);
+
 //   app.setGlobalPrefix(PREFIX);
 //   await app.listen(PORT, () => {
 //     logger.log(`服务已经启动,接口请访问:http://wwww.localhost:${PORT}/${PREFIX}`);
 //   });
 // }
+async function bootstrap() {
+  const logger: Logger = new Logger('main.ts');
+  //暂时关闭了api接口
+const app = await NestFactory.create( AppModule , {
+  // 开启日志级别打印
+  logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
+});
+  //允许跨域请求
+  app.enableCors();
+  // 启动版本管理
+  app.enableVersioning({
+    defaultVersion: '1', // 不指定默认版本为v1
+    type: VersioningType.URI,
+  });
+
+  // 给请求添加prefix
+  app.setGlobalPrefix(PREFIX);
+  await app.listen(PORT, () => {
+    logger.log(`服务已经启动,接口请访问:http://wwww.localhost:${PORT}/${PREFIX}`);
+  });
+}
 bootstrap();
