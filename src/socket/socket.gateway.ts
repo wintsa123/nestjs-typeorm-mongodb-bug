dcf6cc85c37cbd9b3c0bbc87613dc3c66ecf41d5
wintsa <wintsa123@gmail.com>
2023-11-21 11:18:42 +0800

    1121

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,10 +1,18 @@
 import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
-
+import { WsServiceResponseInterceptor } from "src/interceptors/websock.interceptor";
 import { SocketService } from './socket.service';
 import { CreateSocketDto } from './dto/create-socket.dto';
 import { UpdateSocketDto } from './dto/update-socket.dto';
+import { UseFilters, UseInterceptors } from '@nestjs/common';
+import { WsServiceExceptionFilter } from '@src/filters/socket.filter';
+@UseInterceptors(new WsServiceResponseInterceptor())
+@UseFilters(new WsServiceExceptionFilter())
+
 @WebSocketGateway(9088, {
-  transports: ['websocket']
+  transports: ['websocket'],
+  cors: {
+    origin: '*',
+},
 })
 export class SocketGateway {
   constructor(private readonly socketService: SocketService) {}
