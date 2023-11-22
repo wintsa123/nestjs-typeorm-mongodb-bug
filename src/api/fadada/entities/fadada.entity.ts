fa6068f077c53edf6b7d72613c5ddb5c7ab86c9e
wintsa <770775984@qq.com>
2023-11-22 10:23:23 +0800

    sql

--- src/api/fadada/entities/fadada.entity.ts
+++ src/api/fadada/entities/fadada.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { SharedEntity } from '@src/shared/entities/base.entity';
 
-@Entity({ name: 'fadadaAccount' }) // 指定表名为 'MyUsers'
+@Entity({ name: 'fadada' }) // 指定表名为 'MyUsers'
 
 export class Fadada extends SharedEntity {
     @Column({ unique: true }) // 设置唯一性约束，确保 clientUserId 是唯一的
