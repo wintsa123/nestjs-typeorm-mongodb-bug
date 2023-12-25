4db2323db08594d0113785cf401f5dac4907416e
wintsa <wintsa123@gmail.com>
2023-12-25 14:22:14 +0800

    backup  2023/12/25 周一 14:22:14.66

--- src/api/zhiyin/zhiyin.service.ts
+++ src/api/zhiyin/zhiyin.service.ts
@@ -360,10 +360,10 @@ export class ZhiyinService {
       if (opApplyDetailRequest.id == 0) {
         //管理员无条件发起
         delete opApplyDetailRequest['id']
-        let applyData = await this.applyDetailRepository.findOne({ where: { createOaUserName: '管理员无审批盖章'} });
+        let applyData = await this.applyDetailRepository.findOne({ where: { createOaUserName: '管理员无审批盖章' } });
 
 
-
+        console.log(applyData)
         if (!applyData) {
 
           if (!!opApplyDetailRequest['stampOpenUserId']) {
