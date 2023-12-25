d4e2a5e6a59cda94bb25851bf6f8f23ddef84bcf
wintsa <wintsa123@gmail.com>
2023-12-25 11:34:15 +0800

    backup  2023/12/25 周一 11:34:15.60

--- src/api/fadada/dto/fadadaList.dto.ts
+++ src/api/fadada/dto/fadadaList.dto.ts
@@ -1,12 +1,20 @@
-import { Type } from 'class-transformer';
-import { IsInt, IsNotEmpty, IsOptional, MaxLength, Min } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-export class fadadaList {
-    @ApiProperty({ required: true })
-    @IsNotEmpty()
-    openId!: string
-    @ApiProperty({ required: true })
-    @IsNotEmpty()
-    idType!: string
+import { PartialType, ApiProperty } from '@nestjs/swagger';
+
+export class FileProcess extends PartialType(class { }) {
+    @ApiProperty({
+        type: 'object', properties: {
+            idType: { type: 'string' },
+            openId: { type: 'string' },
+
+        }, required: true
+    })
+    OpenId!: OpenId[];
+}
+
+interface OpenId {
+    /** 文件的用途类型: doc | attach */
+    idType: string;
+    /** 法大大云存储中的源文件地址 */
+    openId: string;
 
 }
