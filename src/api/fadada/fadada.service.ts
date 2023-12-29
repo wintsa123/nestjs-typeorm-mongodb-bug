5a4254a7305e5b80a259cce04d12b2457dbd652e
wintsa <wintsa123@gmail.com>
2023-12-29 11:57:12 +0800

    backup  2023/12/29 周五 11:57:12.48

--- src/api/fadada/fadada.service.ts
+++ src/api/fadada/fadada.service.ts
@@ -10,7 +10,6 @@ import { ConfigService } from '@nestjs/config';
 import { RedisService } from '@src/plugin/redis/redis.service';
 import { SocketGateway } from 'src/socket/socket.gateway';
 import { fadadafree } from './entities/fadadaFree.entity';
-import { formatParams, formatSignString, sign } from '@src/utils';
 import { signTaskStatus } from '@src/enums';
 
 @Injectable()
@@ -59,8 +58,8 @@ export class FadadaService {
         serverUrl: this.configService.get('fadada.serverUrl') as string,
       })
       const token: any = await client.getAccessToken()
- 
-      if (token.status !== 200 || token.data.code !== '100000' ) {
+
+      if (token.status !== 200 || token.data.code !== '100000') {
         this.logger.error('Token获取失败')
         throw token.data
       }
@@ -98,7 +97,6 @@ export class FadadaService {
       return 'success'
     }
 
-    const fadada = require('@fddnpm/fasc-openapi-node-sdk');
     const params = {
       "X-FASC-App-Id": headers['X-FASC-App-Id'],
       "X-FASC-Sign-Type": headers['X-FASC-Sign-Type'],
@@ -108,7 +106,7 @@ export class FadadaService {
       bizContent: headers['bizContent'],
       appSecret: this.configService.get('fadada.appSecret')
     }
-    const sign = fadada.utils.sign(params);
+    const sign = fascOpenApi.utils.sign(params);
 
     if (sign !== headers['X-FASC-Sign']) {
       this.logger.error('法大大回调验证出错')
